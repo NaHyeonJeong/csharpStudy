f18e5e8782ee1f12b29cd48ca17539a1fa8dc944
pigismile <pigismile@gmail.com>
2024-05-05 22:31:33 +0900

    2024-05-05 오늘 공부 끝

--- StringModifyApp/StringModify.cs
+++ StringModifyApp/StringModify.cs
@@ -9,12 +9,12 @@ namespace StringModifyApp
             WriteLine("ToLower() : {0}", "ABC".ToLower());
             WriteLine("ToUpper() : {0}", "abc".ToUpper());
 
-            WriteLine("ToLower() : {0}", "ABC".ToLower());
-            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("Insert() : '{0}'", "회사 업무가 기초부터 공부 중!".Insert(7, "지겨워져서 "));
+            WriteLine("Remove() : '{0}'", "I don't Love you".Remove(2,6));
 
-            WriteLine("ToLower() : {0}", "ABC".ToLower());
-            WriteLine("ToLower() : {0}", "ABC".ToLower());
-            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("Trim() : '{0}'", "지겨워! 지겹다구! ".Trim());
+            WriteLine("TrimStart() : '{0}'", " 지겨워! 지겹다구! ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", "지겨워! 지겹다구! ".TrimEnd());
         }
     }
 }
---
+++ StringSliceApp/StringSlice.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+namespace StringSliceApp
+{
+    class StringSlice
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good Morning.";
+            WriteLine(greeting.Substring(0,5)); // Good
+            WriteLine(greeting.Substring(5));   // Morning
+            WriteLine();
+
+            string[] arr = greeting.Split(new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length); // 공백 기준으로 나누면 단어는 2개 나옴
+
+            foreach (string item in arr)
+            {
+                WriteLine(item);
+            }
+        }
+    }
+}
