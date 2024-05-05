1ab855c68751d4f556895039dc825e47c58e5e6c
pigis <pigis@PIGGY_NOTE>
2024-05-05 20:33:47 +0900

    초기 커밋.

---
+++ StringModifyApp/StringModify.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace StringModifyApp
+{
+    class StringModify
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("ToUpper() : {0}", "abc".ToUpper());
+
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+            WriteLine("ToLower() : {0}", "ABC".ToLower());
+        }
+    }
+}
